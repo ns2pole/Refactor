a737bcb6c3190b5b4d340b410f90d81a8690a208
ns2pole <spaceofstar2@gmail.com>
2024-06-17 03:18:31 +0900

    committest

---
+++ src/main/java/org/example/Sample1After.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Sample1After {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        addAndPrint(a, b, "Sum1: ");
+        minusAndPrint(a, b, "Difference1: ");
+
+        int x = 15;
+        int y = 7;
+
+        addAndPrint(x, y, "Sum2: ");
+        minusAndPrint(x, y, "Difference2: ");
+    }
+
+    public static int addAndPrint(int a, int b, String message) {
+        int sum = a + b;
+        System.out.println(message + sum);
+        return sum;
+    }
+
+    public static int minusAndPrint(int a, int b, String message) {
+        int diff = a - b;
+        System.out.println(message + diff);
+        return diff;
+    }
+}
---
+++ src/main/java/org/example/Sample1Before.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Sample1Before {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        // a と b の和を計算
+        int sum1 = a + b;
+        System.out.println("Sum1: " + sum1);
+
+        // a と b の差を計算
+        int diff1 = a - b;
+        System.out.println("Difference1: " + diff1);
+
+        int x = 15;
+        int y = 7;
+
+        // x と y の和を計算
+        int sum2 = x + y;
+        System.out.println("Sum2: " + sum2);
+
+        // x と y の差を計算
+        int diff2 = x - y;
+        System.out.println("Difference2: " + diff2);
+    }
+}
